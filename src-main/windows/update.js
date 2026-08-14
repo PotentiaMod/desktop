858a044c04875d3d2cb0ccdc58154f7450d97f96
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-08-14 10:14:30 -0400

    Update update.js

--- src-main/windows/update.js
+++ src-main/windows/update.js
@@ -31,7 +31,7 @@ class UpdateWindow extends AbstractWindow {
       const params = new URLSearchParams();
       params.set('from', currentVersion);
       params.set('to', latestVersion);
-      openExternal(`https://desktop.turbowarp.org/update_available?${params}`);
+      openExternal(`https://potentiamod.github.io/pot-desktop/update_available?${params}`);
     });
 
     const ignore = (permanently) => {
